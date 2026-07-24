7cabcb5f6f41a7089576636636535751d2995d3a
l <96929613+AdrenalineGremlin@users.noreply.github.com>
2026-07-24 16:41:00 -0400

    repo interfaces finished

--- src/main/java/mls_data_classification_engine/mls_engine/repository/ClassifiedDocumentRepository.java
+++ src/main/java/mls_data_classification_engine/mls_engine/repository/ClassifiedDocumentRepository.java
@@ -1,5 +1,14 @@
 package mls_data_classification_engine.mls_engine.repository;
 
-public class ClassifiedDocumentRepository {
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import mls_data_classification_engine.mls_engine.model.ClassificationLevel;
+import mls_data_classification_engine.mls_engine.model.ClassifiedDocument;
+import java.util.List;
+
+public interface ClassifiedDocumentRepository extends JpaRepository<ClassifiedDocument, UUID> {
+
+    // list instead of optional to return the list of classified documents
+    List<ClassifiedDocument> findByClassificationOfDocument(ClassificationLevel classification);
 }
--- src/main/java/mls_data_classification_engine/mls_engine/repository/UserRepository.java
+++ src/main/java/mls_data_classification_engine/mls_engine/repository/UserRepository.java
@@ -1,5 +1,11 @@
 package mls_data_classification_engine.mls_engine.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+import mls_data_classification_engine.mls_engine.model.User;
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, UUID> {
+    // optional used for either the username exists or doesnt
+    Optional<User> findByUsername(String username);
 }
