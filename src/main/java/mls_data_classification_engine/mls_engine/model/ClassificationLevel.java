3954086a459c0458d8f8a8fa11f191a547dea803
l <96929613+AdrenalineGremlin@users.noreply.github.com>
2026-07-24 15:06:48 -0400

    classification level done

--- src/main/java/mls_data_classification_engine/mls_engine/model/ClassificationLevel.java
+++ src/main/java/mls_data_classification_engine/mls_engine/model/ClassificationLevel.java
@@ -1,5 +1,18 @@
 package mls_data_classification_engine.mls_engine.model;
 
-public class ClassificationLevel {
+public enum ClassificationLevel {
+    UNCLASSIFIED,
+    CONFIDENTIAL,
+    SECRET,
+    TOP_SECRET;
 
+    // these will determine wether to read
+    public boolean isAtLeast(ClassificationLevel other) {
+        return this.ordinal() >= other.ordinal();
+    }
+
+    // or write
+    public boolean isAtMost(ClassificationLevel other) {
+        return this.ordinal() <= other.ordinal();
+    }
 }
