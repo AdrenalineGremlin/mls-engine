dc700635dc1dab74e58c1e07c59505c4fe9ac470
l <96929613+AdrenalineGremlin@users.noreply.github.com>
2026-07-24 15:43:50 -0400

    User entity databse completed

--- src/main/java/mls_data_classification_engine/mls_engine/model/User.java
+++ src/main/java/mls_data_classification_engine/mls_engine/model/User.java
@@ -1,5 +1,39 @@
 package mls_data_classification_engine.mls_engine.model;
 
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
 public class User {
+    /*
+     * Id
+     * Username
+     * password *stored as hash never plain text
+     * clearerence level
+     */
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Enumerated(EnumType.STRING)
+    private ClassificationLevel clearanceLevel;
+
+    private String username;
+    private String passwordHash;
 
 }
