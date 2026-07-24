1013d4bb6324cbdfc2c7b87273f718e2e3658fa3
l <96929613+AdrenalineGremlin@users.noreply.github.com>
2026-07-24 18:45:37 -0400

    create AccesType enum and started working on accessdecisionservice

---
+++ src/main/java/mls_data_classification_engine/mls_engine/model/AccessType.java
@@ -0,0 +1,6 @@
+package mls_data_classification_engine.mls_engine.model;
+
+public enum AccessType {
+    READ,
+    WRITE;
+}
--- src/main/java/mls_data_classification_engine/mls_engine/security/AccessDecisionService.java
+++ src/main/java/mls_data_classification_engine/mls_engine/security/AccessDecisionService.java
@@ -1,5 +1,11 @@
 package mls_data_classification_engine.mls_engine.security;
 
-public class AccessDecisionService {
+import mls_data_classification_engine.mls_engine.model.AccessType;
+import mls_data_classification_engine.mls_engine.model.ClassifiedDocument;
+import mls_data_classification_engine.mls_engine.model.User;
 
+public class AccessDecisionService {
+    public boolean hasAccess(User user, ClassifiedDocument document, AccessType access) {
+
+    }
 }
