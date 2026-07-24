7912677e7e6f17077c5dddf6c3942f6847fff50e
l <96929613+AdrenalineGremlin@users.noreply.github.com>
2026-07-24 16:05:48 -0400

    finished document Entity

--- src/main/java/mls_data_classification_engine/mls_engine/model/ClassifiedDocument.java
+++ src/main/java/mls_data_classification_engine/mls_engine/model/ClassifiedDocument.java
@@ -1,5 +1,42 @@
 package mls_data_classification_engine.mls_engine.model;
 
-public class ClassifiedDocument {
+import java.util.UUID;
 
+import java.time.Instant;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
+public class ClassifiedDocument {
+    /*
+     * content
+     * title
+     * document id
+     * classification level
+     * timstamp
+     */
+
+    @Id
+    @GeneratedValue
+    private UUID documentId;
+
+    @Enumerated(EnumType.STRING)
+    private ClassificationLevel classificationOfDocument;
+    private String title;
+    private String content;
+
+    private Instant createdAt;
 }
