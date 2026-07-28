ac711d8019880d015055deb37f1b66ae90409928
l <96929613+AdrenalineGremlin@users.noreply.github.com>
2026-07-28 16:20:56 -0400

    finished accessdecision

--- src/main/java/mls_data_classification_engine/mls_engine/security/AccessDecisionService.java
+++ src/main/java/mls_data_classification_engine/mls_engine/security/AccessDecisionService.java
@@ -7,5 +7,22 @@ import mls_data_classification_engine.mls_engine.model.User;
 public class AccessDecisionService {
     public boolean hasAccess(User user, ClassifiedDocument document, AccessType access) {
 
+        // the action of acess gets the accesstype, if its read
+        // system gets user clearancelevel and compares if its atleast up to document
+        // classfication
+        if (access == AccessType.READ) {
+            return user.getClearanceLevel().isAtLeast(document.getClassificationOfDocument());
+
+        }
+        // the action of acess gets the accesstype, if its read
+        // system gets user clearancelevel and compares if its atleast up to document
+        // classfication
+        if (access == AccessType.WRITE) {
+            return user.getClearanceLevel().isAtMost(document.getClassificationOfDocument());
+        }
+
+        // return false as a security fail safe
+        return false;
+
     }
 }
