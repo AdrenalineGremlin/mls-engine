34210a0a9cdaa4526ed13678aa711d69e4cebfec
l <96929613+AdrenalineGremlin@users.noreply.github.com>
2026-08-02 15:03:50 -0400

    finished filter for jwt auth

--- src/main/java/mls_data_classification_engine/mls_engine/security/JwtAuthFilter.java
+++ src/main/java/mls_data_classification_engine/mls_engine/security/JwtAuthFilter.java
@@ -1,5 +1,50 @@
 package mls_data_classification_engine.mls_engine.security;
 
-public class JwtAuthFilter {
+import java.io.IOException;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.util.Collections;
+import mls_data_classification_engine.mls_engine.model.User;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import mls_data_classification_engine.mls_engine.repository.UserRepository;
+
+// intecepts requests, verifies if token is genuine, and extracts the username
+// and this registers identiy with spring security context
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    // inherated abstract method
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        // get auth
+        String authHeader = request.getHeader("Authorization");
+        // bearer is the format label that stores token
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        // verifies token is valid
+        String token = authHeader.substring(7);
+
+        String username = jwtService.extractUsername(token);
+        User user = userRepository.findByUsername(username).orElseThrow();
+        // treat as a logged in user
+        SecurityContextHolder.getContext()
+                .setAuthentication(new UsernamePasswordAuthenticationToken(user, null, Collections.emptyList()));
+        filterChain.doFilter(request, response);
+    }
+
+    public JwtAuthFilter(JwtService jwtService, UserRepository userRepository) {
+        this.jwtService = jwtService;
+        this.userRepository = userRepository;
+    }
 }
