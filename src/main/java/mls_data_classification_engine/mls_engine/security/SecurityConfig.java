54e41b9070d75cc50f4b2add9056afe611c35baf
l <96929613+AdrenalineGremlin@users.noreply.github.com>
2026-08-02 22:45:19 -0400

    explanation

--- src/main/java/mls_data_classification_engine/mls_engine/security/SecurityConfig.java
+++ src/main/java/mls_data_classification_engine/mls_engine/security/SecurityConfig.java
@@ -9,11 +9,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
+// to tell spring security how to handle request
 @Configuration
 public class SecurityConfig {
     private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
+    // wherever hashing or password check is needed for injection
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
@@ -21,10 +23,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         // targets login and register for non authentication and catches everything else
+        // turn off csrf because no cookies are being used
         http.csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/login", "/register")
                         .permitAll().anyRequest().authenticated())
+                // register jaf into filter chain
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
