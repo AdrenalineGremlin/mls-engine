5276d9700d33e3e71e3cf17032a81fcfe49aa2a7
l <96929613+AdrenalineGremlin@users.noreply.github.com>
2026-08-03 15:15:09 -0400

    wrote write function

--- src/main/java/mls_data_classification_engine/mls_engine/service/DocumentService.java
+++ src/main/java/mls_data_classification_engine/mls_engine/service/DocumentService.java
@@ -19,13 +19,23 @@ public class DocumentService {
     public ClassifiedDocument readDocument(User user, UUID id) {
         // get docu by id
         ClassifiedDocument document = classifiedDocumentRepository.findById(id).orElseThrow();
-        // check if user can access
+        // check if user can access to read
         boolean access = accessDecisionService.hasAccess(user, document, AccessType.READ);
         // if so return document
         if (access) {
             return document;
         }
-        throw new RuntimeException("Cannot Access Document");
+        throw new RuntimeException("Cannot Access Document to READ");
+    }
+
+    public ClassifiedDocument writeDocument(User user, ClassifiedDocument document) {
+        // check if user can access to write
+        boolean access = accessDecisionService.hasAccess(user, document, AccessType.WRITE);
+        // if so, save document /// write meaning saving into storage
+        if (access) {
+            return classifiedDocumentRepository.save(document);
+        }
+        throw new RuntimeException("Cannot Access Document to WRITE");
     }
 
     public DocumentService(ClassifiedDocumentRepository classifiedDocumentRepository,
