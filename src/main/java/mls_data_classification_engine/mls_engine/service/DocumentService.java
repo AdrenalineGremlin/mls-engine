7c60f6cb66ab6100a252eaa9462608fca3ac131a
l <96929613+AdrenalineGremlin@users.noreply.github.com>
2026-08-03 14:42:26 -0400

    finished document service

--- src/main/java/mls_data_classification_engine/mls_engine/service/DocumentService.java
+++ src/main/java/mls_data_classification_engine/mls_engine/service/DocumentService.java
@@ -1,5 +1,37 @@
 package mls_data_classification_engine.mls_engine.service;
 
-public class DocumentService {
+import java.util.UUID;
 
+import org.springframework.stereotype.Service;
+import mls_data_classification_engine.mls_engine.repository.ClassifiedDocumentRepository;
+import mls_data_classification_engine.mls_engine.repository.UserRepository;
+import mls_data_classification_engine.mls_engine.security.AccessDecisionService;
+import mls_data_classification_engine.mls_engine.model.AccessType;
+import mls_data_classification_engine.mls_engine.model.ClassificationLevel;
+import mls_data_classification_engine.mls_engine.model.ClassifiedDocument;
+import mls_data_classification_engine.mls_engine.model.User;
+
+@Service
+public class DocumentService {
+    private final ClassifiedDocumentRepository classifiedDocumentRepository;
+    private final AccessDecisionService accessDecisionService;
+
+    public ClassifiedDocument readDocument(User user, UUID id) {
+        // get docu by id
+        ClassifiedDocument document = classifiedDocumentRepository.findById(id).orElseThrow();
+        // check if user can access
+        boolean access = accessDecisionService.hasAccess(user, document, AccessType.READ);
+        // if so return document
+        if (access) {
+            return document;
+        }
+        throw new RuntimeException("Cannot Access Document");
+    }
+
+    public DocumentService(ClassifiedDocumentRepository classifiedDocumentRepository,
+            AccessDecisionService accessDecisionService) {
+        this.classifiedDocumentRepository = classifiedDocumentRepository;
+        this.accessDecisionService = accessDecisionService;
+
+    }
 }
