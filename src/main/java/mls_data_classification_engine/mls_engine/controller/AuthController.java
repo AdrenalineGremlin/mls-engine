57b50489071a2923df74e2bce2c319de5fce3613
l <96929613+AdrenalineGremlin@users.noreply.github.com>
2026-08-03 17:05:21 -0400

    finished auth controller

--- src/main/java/mls_data_classification_engine/mls_engine/controller/AuthController.java
+++ src/main/java/mls_data_classification_engine/mls_engine/controller/AuthController.java
@@ -1,8 +1,39 @@
 package mls_data_classification_engine.mls_engine.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import mls_data_classification_engine.mls_engine.service.AuthService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import mls_data_classification_engine.mls_engine.DTO.AuthRequest;
+import mls_data_classification_engine.mls_engine.DTO.AuthResponse;
+import mls_data_classification_engine.mls_engine.model.User;
 
 @RestController
+// sets shared url prefix for every endpoint
+@RequestMapping("/auth")
 public class AuthController {
+    private final AuthService authService;
 
+    @PostMapping("/register")
+    public String register(@RequestBody AuthRequest request) {
+        // TODO: process POST request
+        User savedUser = authService.registUser(request.getUsername(), request.getRawpassword());
+        return savedUser.getUsername();
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody AuthRequest request) {
+        // TODO: process POST request
+
+        String token = authService.login(request.getUsername(), request.getRawpassword());
+        AuthResponse response = new AuthResponse();
+        response.setToken(token);
+
+        return response;
+    }
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
 }
