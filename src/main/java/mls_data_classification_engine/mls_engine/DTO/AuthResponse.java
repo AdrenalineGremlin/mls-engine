941578d886d98f9a4fa0d8e076fae3504ad7b081
l <96929613+AdrenalineGremlin@users.noreply.github.com>
2026-08-03 15:18:27 -0400

    added the two dtos

---
+++ src/main/java/mls_data_classification_engine/mls_engine/DTO/AuthResponse.java
@@ -0,0 +1,5 @@
+package mls_data_classification_engine.mls_engine.DTO;
+
+public class AuthResponse {
+
+}
--- src/main/java/mls_data_classification_engine/mls_engine/controller/AuthController.java
+++ src/main/java/mls_data_classification_engine/mls_engine/controller/AuthController.java
@@ -1,5 +1,8 @@
 package mls_data_classification_engine.mls_engine.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AuthController {
 
 }
