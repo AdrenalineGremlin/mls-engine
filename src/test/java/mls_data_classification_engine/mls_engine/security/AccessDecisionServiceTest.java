2003c8e8c4b9e190c514a941dbd0d68d7f8e99d4
l <96929613+AdrenalineGremlin@users.noreply.github.com>
2026-08-04 17:03:40 -0400

    created test file for accessdecision

---
+++ src/test/java/mls_data_classification_engine/mls_engine/security/AccessDecisionServiceTest.java
@@ -0,0 +1,13 @@
+package mls_data_classification_engine.mls_engine.security;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import mls_data_classification_engine.mls_engine.security.AccessDecisionService;
+import mls_data_classification_engine.mls_engine.model.User;
+import mls_data_classification_engine.mls_engine.model.ClassifiedDocument;
+import mls_data_classification_engine.mls_engine.model.ClassificationLevel;
+import mls_data_classification_engine.mls_engine.model.AccessType;
+
+public class AccessDecisionServiceTest {
+
+}
